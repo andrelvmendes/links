5ae3911a3b52c2a7d2f973f107af9379bea309bb
André Mendes <develmendes@gmail.com>
2026-08-20 16:47:55 -0300

    feat: adiciona social media

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,4 +1,7 @@
-import { FaGithub, FaGlobe, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { FaCloudShowersHeavy, FaGithub, FaGlobe, FaInstagram, FaLinkedin, FaRocket, FaWeight, FaWeightHanging } from 'react-icons/fa'
+import { FaArrowsLeftRight, FaWeightScale } from 'react-icons/fa6'
+import { GiPowerLightning, GiWeight, GiWeightCrush, GiWeightLiftingDown, GiWeightLiftingUp, GiWeightScale } from 'react-icons/gi'
+import { TbWeight } from 'react-icons/tb'
 
 export type ProfileData = {
   name: string
@@ -34,14 +37,16 @@ export const siteData: SiteData = {
     bio: 'Desenvolvedor Full Stack | Python | FastAPI | Java | Springboot'
   },
   links: [
-    { id: 'portfolio', title: 'Ver meu portfólio', url: 'https://andre-mendes.vercel.app/', category: 'portfolio', },
+    { id: 'portfolio', title: 'Veja mais sobre mim', url: 'https://andre-mendes.vercel.app/', category: 'portfolio', },
     { id: 'linkedin', title: 'Fale comigo no LinkedIn', url: 'https://example.com', category: 'social' },
     { id: 'teams', title: 'Fale comigo pelo Teams', url: 'https://teams.live.com/l/invite/FAAOy0tMUKkpk8cYw?v=g1', category: 'contato' }
   ],
   social: [
     { id: 'github', url: 'https://github.com/andrelvmendes', icon: <FaGithub size={30} /> },
     { id: 'linkedin', url: 'https://www.linkedin.com/in/andrelvmendes/', icon: <FaLinkedin size={30} /> },
-    { id: 'instagram', url: 'https://instagram.com/andrelvmendes', icon: <FaInstagram size={30} /> },
     { id: 'website', url: 'https://andre-mendes.vercel.app/', icon: <FaGlobe size={30} /> },
+    { id: 'rocket', url: 'https://app.rocketseat.com.br/me/andrelvmendes', icon: <FaRocket size={30} /> },
+    { id: 'instagram', url: 'https://instagram.com/andrelvmendes', icon: <FaInstagram size={30} /> },
+    { id: 'hevy', url: 'https://hevy.com/user/andrelvmendes', icon: <GiWeightLiftingUp size={30} /> },
   ]
 }
